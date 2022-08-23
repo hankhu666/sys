3eee64516ab174af8ab0a81100385ad52f4e8277
hankhu666 <26704892@qq.com>
2022-08-24 00:26:50 +0800

    tools

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/mu && chmod +x mu && bash mu
-wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/
 
 
