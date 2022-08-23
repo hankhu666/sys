9b8f8b176aa2b30da9a698acaab80977064f06d3
hankhu666 <26704892@qq.com>
2022-08-24 00:50:23 +0800

    install_ups

---
+++ install_ups.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/tools/ups && chmod +x mu && mv ups /usr/local/sbin/ && ln -s /usr/local/sbin/ /usr/local/bin/
+
