bb85deae593c275f7f66d30e917b314efc279cd0
hankhu666 <26704892@qq.com>
2022-08-24 00:58:32 +0800

    install_ups

--- install_ups.sh
+++ install_ups.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/tools/ups && chmod +x ups && mv ups /usr/local/sbin/ && ln -s /usr/local/sbin/ /usr/local/bin/
-
+if [ $? ]
+then
+    echo install ups succesed
+fi
