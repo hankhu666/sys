2f848434bdcff9b3902ef76b1b2adb6363e00c5d
hankhu666 <26704892@qq.com>
2022-08-24 13:09:35 +0800

    mu/rm

--- install_sys.sh
+++ install_sys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/tools/musys && chmod +x musys && bash musys 
+wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/musys && chmod +x musys && bash musys 
 if [ $? ]
 then
     echo install musys succesed
--- remove_sys.sh
+++ remove_sys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/tools/rmsys && chmod +x rmsys && bash rmsys 
+wget -N --no-check-certificate https://raw.githubusercontent.com/hankhu666/sys/main/rmsys && chmod +x rmsys && bash rmsys 
 if [ $? ]
 then
     echo  sysmain removed
